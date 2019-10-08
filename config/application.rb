bedcfc7fdafe9786a00a12c88c5007abecc6c75b
anthonymarze <anthonymarze@gmail.com>
2019-10-08 14:58:34 -0400

    added basic route dragging functionality and styled map 80%

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,10 @@ module Struggle
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
--- frontend/components/map/map.jsx
+++ frontend/components/map/map.jsx
@@ -6,58 +6,108 @@ class Map extends React.Component {
     constructor(props) {
         super(props);
         this.calculateAndDisplayRoute = this.calculateAndDisplayRoute.bind(this);
-        this.showSteps = this.showSteps.bind(this);
+        this.setMapOnAll = this.setMapOnAll.bind(this);
+        this.showMarkers = this.showMarkers.bind(this);
+        this.addMarker = this.addMarker.bind(this);
+        this.deleteMarkers = this.deleteMarkers.bind(this);
+        this.clearMarkers = this.clearMarkers.bind(this);
+        this.listenForClick = this.listenForClick.bind(this);
+        this.markers = [];
+        this.waypts = [];
     }
 
     componentDidMount() {
         const map = ReactDOM.findDOMNode(this.refs.map)
         const options = {
-            center: { lat: 44.3064688, lng: -76.0005896 },
+            center: { lat: 40.751370, lng: - 73.983982 },
             zoom: 17
         };
-        let markerArray = [];
-        let directionsService = new google.maps.DirectionsService();
+        this.directionsService = new google.maps.DirectionsService();
         this.map = new google.maps.Map(map, options);
-        let directionsRenderer = new google.maps.DirectionsRenderer({ map: this.map });
+        this.directionsRenderer = new google.maps.DirectionsRenderer({
+            draggable: true,
+            map: this.map
+        });
         
         this.calculateAndDisplayRoute(
-            directionsRenderer, directionsService, markerArray, this.map);
+            this.directionsRenderer, this.directionsService);
         
         this.listenForMove();
-        directionsService.route({
-
+        this.listenForClick();
+        this.listenForDirectionsChange();
+        this.labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        this.labelIndex = 0;
+    }
+    
+    addMarker(location, map) {
+        let marker = new google.maps.Marker({
+            position: location,
+            label: this.labels[this.labelIndex++ % this.labels.length],
+            map: map,
+        })
+        this.markers.push(marker)
+        this.waypts.push({
+            location: marker.getPosition(),
+            stopover: false
         });
     }
 
-    calculateAndDisplayRoute(directionsRenderer, directionsService, markerArray, map) {
-        for (var i = 0; i < markerArray.length; i++) {
-            markerArray[i].setMap(null);
+    setMapOnAll(map) {
+        for (let i = 0; i < this.markers.length; i++) {
+            this.markers[i].setMap(map);
         }
+    }
 
+    clearMarkers() {
+        this.setMapOnAll(null);
+    }
+
+    showMarkers() {
+        this.setMapOnAll(this.map);
+    }
+    
+    deleteMarkers() {
+        this.clearMarkers();
+        this.markers = [];
+        this.waypts = [];
+        debugger
+        this.calculateAndDisplayRoute(this.directionsRenderer, this.directionsService)
+        debugger
+    }
+
+    calculateAndDisplayRoute(directionsRenderer, directionsService) {
         directionsService.route({
-            origin: { lat: 44.30961331926814, lng: -75.99316013689946 },
-            destination: { lat: 44.30270334916442, lng: -76.00879205103826 },
+            origin: { lat: 40.751370, lng: - 73.983982 },
+            destination: { lat: 40.732490, lng: - 73.991023 },
+            waypoints: this.waypts,
             travelMode: 'WALKING',
         }, (response, status) => {
             if (status === 'OK') {
                 document.getElementById('warnings-panel').innerHTML =
                     '<b>' + response.routes[0].warnings + '<b>';
                 directionsRenderer.setDirections(response);
-                this.showSteps(response, markerArray, map);
             } else {
                 window.alert('Directions request failed due to ' + status);
             }
         });
     }
 
-    showSteps(directionsResult, markerArray, map) {
-        let myRoute = directionsResult.routes[0].legs[0];
-        for (var i = 0; i < myRoute.steps.length; i++) {
-            let marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
-            marker.setMap(map);
-            marker.setPosition(myRoute.steps[i].start_location);
+    computeTotalDistance(result) {
+        let total = 0;
+        let myroute = result.routes[0];
+        for (let i = 0; i < myroute.legs.length; i++) {
+          total += myroute.legs[i].distance.value;
         }
+        total = total / 1000;
+        document.getElementById('total').innerHTML = total + ' km';
     }
+
+    listenForClick() {
+        google.maps.event.addListener(this.map, 'click', (event) => {
+            this.addMarker(event.latLng, this.map);
+            this.calculateAndDisplayRoute(this.directionsRenderer, this.directionsService);
+        })
+    } 
     
     listenForMove() {
         google.maps.event.addListener(this.map, 'idle', () => {
@@ -75,11 +125,29 @@ class Map extends React.Component {
         });
     }
 
+    listenForDirectionsChange() {
+        this.directionsRenderer.addListener('directions_changed', () => {
+            this.computeTotalDistance(this.directionsRenderer.getDirections());
+        });
+    }
+
     render() {
         return (
-            <div>
+            <div id="map-display-container">
+                <div id="top-panel">
+                    <div className="map-inputs">
+                        <button onClick={this.clearMarkers}>Hide Markers</button>
+                        <button onClick={this.showMarkers}>Show All Markers</button>
+                        <button onClick={this.deleteMarkers}>Delete Markers</button>
+                    </div>
+                    <input id="save-btn" type="submit" value="save"></input>
+                </div>
                 <div id="warnings-panel"></div> 
+                <div id="bottom-panel">
+                    <p>Total Distance: <span id="total"></span></p>
+                </div>
                 <div id="map" ref="map" />
+                
             </div>
         );
     }
