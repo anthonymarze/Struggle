6b7f8eb795571e412828a2d30060ed453d38ad3f
anthonymarze <anthonymarze@gmail.com>
2019-10-11 10:31:52 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(email: 'user@demo.edu',
+            password: 'password',
+            first_name: 'Demo',
+            last_name: 'User',
+            birthday: Date.new(2000,1,1),
+            gender: 'Female',)
